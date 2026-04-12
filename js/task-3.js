a0a9bc5d3213be8c06565b78025ff9ef830d78d8
Diana Muzhylivska <diana.listen@gmail.com>
2026-04-13 00:01:25 +0300

    add ht #5

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,58 @@
-function checkForSpam(message) {
-  const lowerMessage = message.toLowerCase();
-  if (lowerMessage.includes('spam') || lowerMessage.includes('sale')) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// Напиши стрілочну функцію sortByDescendingFriendCount(users),
+// яка прийматиме один параметр users — масив об’єктів користувачів.
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+// Функція має повертати масив усіх користувачів,
+// відсортованих за спаданням кількостій їх друзів(властивість friends).
+
+const sortByDescendingFriendCount = users => {
+  return users.toSorted((a, b) => b.friends.length - a.friends.length);
+};
+// а якби був порядок ЗРОСТАННЯ? Тоді users.toSorted((a, b) => a.friends.length - b.friends.length);
+
+// Коли ми пишемо b.friends.length - a.friends.length, ми кажемо:
+
+// "Візьми кількість друзів у другого користувача (b) і відніми кількість друзів першого (a)".
+
+// Якщо результат від'ємний — a ставиться перед b.
+// Якщо результат додатний — b ставиться перед a.
+// Якщо 0 — порядок не змінюється.
+
+console.log(
+  getUserNames([
+    {
+      name: 'Moore Hensley',
+      email: 'moorehensley@indexia.com',
+      balance: 2811,
+    },
+    {
+      name: 'Sharlene Bush',
+      email: 'sharlenebush@tubesys.com',
+      balance: 3821,
+    },
+    {
+      name: 'Ross Vazquez',
+      email: 'rossvazquez@xinware.com',
+      balance: 3793,
+    },
+    {
+      name: 'Elma Head',
+      email: 'elmahead@omatom.com',
+      balance: 2278,
+    },
+    {
+      name: 'Carey Barr',
+      email: 'careybarr@nurali.com',
+      balance: 3951,
+    },
+    {
+      name: 'Blackburn Dotson',
+      email: 'blackburndotson@furnigeer.com',
+      balance: 1498,
+    },
+    {
+      name: 'Sheree Anthony',
+      email: 'shereeanthony@kog.com',
+      balance: 2764,
+    },
+  ])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
--- js/task-4.js
+++ js/task-4.js
@@ -1,21 +1,68 @@
-function getShippingCost(country) {
-  switch (country) {
-    case 'China':
-      return `Shipping to ${country} will cost 100 credits`;
-    case 'Chile':
-      return `Shipping to ${country} will cost 250 credits`;
-    case 'Australia':
-      return `Shipping to ${country} will cost 170 credits`;
-    case 'Jamaica':
-      return `Shipping to ${country} will cost 120 credits`;
-    default:
-      return `Sorry, there is no delivery to your country`;
-  }
-}
+// Напиши стрілочну функцію getTotalBalanceByGender(users, gender), яка прийматиме два параметра:
 
-console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost('China')); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost('Chile')); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost('Jamaica')); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost('Sweden')); // "Sorry, there is no delivery to your country"
+// перший параметр users — масив об’єктів користувачів,
+// другий параметр gender — рядок, що зберігає стать.
+// Функція має використовувати ланцюжок виклику методів та
+// повертати загальний баланс користувачів(властивість balance),
+// стать яких(властивість gender) збігається зі значенням параметра gender.
+
+// Мій працюючий код, але тут немає необхідного ланцюжка методів
+// const getTotalBalanceByGender = (users, gender) => {
+//   let balace = 0;
+//   users.filter(user => (balance += user.gender.includes(gender)));
+//   return balance;
+// };
+
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter(user => user.gender === gender) // 1. Фільтруємо користувачів за статтю
+    .reduce((total, user) => total + user.balance, 0); // 2. Сумуємо їхні баланси
+
+// Чому ми змінили код:
+// filter: ми використовуємо його за призначенням — щоб залишити в масиві лише тих людей,
+// чия стать збігається з аргументом gender.
+
+// reduce: цей метод ідеально підходить для "згортання" масиву в одне число(суму).
+// Він приймає акумулятор(total) та поточний об'єкт (user). 0 в кінці — це початкове значення суми.
+
+const clients = [
+  {
+    name: 'Moore Hensley',
+    gender: 'male',
+    balance: 2811,
+  },
+  {
+    name: 'Sharlene Bush',
+    gender: 'female',
+    balance: 3821,
+  },
+  {
+    name: 'Ross Vazquez',
+    gender: 'male',
+    balance: 3793,
+  },
+  {
+    name: 'Elma Head',
+    gender: 'female',
+    balance: 2278,
+  },
+  {
+    name: 'Carey Barr',
+    gender: 'male',
+    balance: 3951,
+  },
+  {
+    name: 'Blackburn Dotson',
+    gender: 'male',
+    balance: 1498,
+  },
+  {
+    name: 'Sheree Anthony',
+    gender: 'female',
+    balance: 2764,
+  },
+];
+
+console.log(getTotalBalanceByGender(clients, 'male')); // 12053
+
+console.log(getTotalBalanceByGender(clients, 'female')); // 8863
